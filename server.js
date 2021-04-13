612ddccebe746000cf85c0fb32679ec3fa0a1714
Arseniy Tyurin <arseniy.ny@gmail.com>
2021-04-12 23:45:58 -0400

    cleanups

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ let USERS = {}
 
 // Start Express Server
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
 // Start Websocket server
@@ -21,64 +21,52 @@ const wss = new websocket.Server({server});
 wss.on('connection', handlingClients);
 
 function handlingClients(ws) {
+  ws.on('message', (message) => {
+    if (typeof message == 'string') {
+      let data = parse(message);
+      switch (data['action']) {
 
-    ws.on('message', (message) => {
+      case 'init':
+        ws.userid = data['userid'];
+        USERS[data['userid']] = {'username': data['username'], 'color': data['color'], 'width': data['width']};
+        console.log(`User ${USERS[data['userid']]['username']} connected`);
+        wss.clients.forEach((client) => { client.send(stringify({'action': 'loadUsers', 'users': USERS})); });
+        break;
 
-      if (typeof message == 'string') {
-        let data = parse(message);
-
-
-        switch (data['action']) {
-
-          case 'init':
-            ws.userid = data['userid'];
-            USERS[data['userid']] = {'username': data['username'], 'color': data['color'], 'width': data['width']};
-            console.log(`User ${USERS[data['userid']]['username']} connected`);
-            wss.clients.forEach((client) => { client.send(stringify({'action': 'loadUsers', 'users': USERS})); });
-            break;
-
-          case 'changeColor':
-            USERS[data['userid']]['color'] = data['color'];
-            USERS[data['userid']]['width'] = data['width'];
-            wss.clients.forEach((client) => {
-              client.send(stringify(
-                {
-                  'action': 'changeColor',
-                  'userid': data['userid'],
-                  'color': data['color'],
-                  'width': data['width']
-                }
-              ));
-              }
-            );
-            break;
-
-        }
-
-      } else {
-        // work with binary array
-        wss.clients.forEach((client) => { client.send(message) });
-        //let x = (message[1] << 8) + message[0];
-        //let y = (message[3] << 8) + message[2];
+      case 'changeColor':
+        USERS[data['userid']]['color'] = data['color'];
+        USERS[data['userid']]['width'] = data['width'];
+        wss.clients.forEach((client) => {
+          client.send(stringify({
+            'action': 'changeColor',
+            'userid': data['userid'],
+            'color': data['color'],
+            'width': data['width']
+          }));
+        });
+        break;
       }
-    });
 
-    ws.on('close', () => {
-      wss.clients.forEach( (client) => {
-        client.send(stringify(
-            {
-              'action': 'deleteUser',
-              'userid': ws.userid
-            }
-          )
-        )
+    } else {
+      // work with binary array
+      wss.clients.forEach((client) => { client.send(message) });
+      //let x = (message[1] << 8) + message[0];
+      //let y = (message[3] << 8) + message[2];
+      }
+  });
+
+  ws.on('close', () => {
+    wss.clients.forEach( (client) => {
+        client.send(stringify({
+          'action': 'deleteUser',
+          'userid': ws.userid
+        }));
       });
-      console.log(`User ${USERS[ws.userid]['username']} (${ws.userid}) disconnected`);
-      delete USERS[ws.userid];
-      console.log(`Total users: ${Object.keys(USERS).length}`);
-      console.log(`Total WS: ${wss.clients.size}`);
-      console.log(USERS);
-
-    });
+    console.log(`User ${USERS[ws.userid]['username']} (${ws.userid}) disconnected`);
+    delete USERS[ws.userid];
+    console.log(`Total users: ${Object.keys(USERS).length}`);
+    console.log(`Total WS: ${wss.clients.size}`);
+    console.log(USERS);
+  });
 
 }
